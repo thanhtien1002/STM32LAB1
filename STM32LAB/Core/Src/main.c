fc44702b01ff87779f35101186c8004433f82173
DESKTOP-M78FRR5\ASUS <caothanhtien2004@gmail.com>
2024-09-24 17:07:00 +0700

    init Ex_1

--- STM32LAB/Core/Src/main.c
+++ STM32LAB/Core/Src/main.c
@@ -24,6 +24,7 @@
 /* USER CODE BEGIN Includes */
 #include <EX.h>
 /* USER CODE END Includes */
+
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
 
