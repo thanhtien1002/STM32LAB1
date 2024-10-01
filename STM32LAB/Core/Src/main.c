8c459957139aa7fbc987b924ac05711c4e398022
DESKTOP-M78FRR5\ASUS <caothanhtien2004@gmail.com>
2024-10-01 15:30:30 +0700

    no message

--- STM32LAB/Core/Src/main.c
+++ STM32LAB/Core/Src/main.c
@@ -21,6 +21,7 @@
 #include "main.h"
 
 /* Private includes ----------------------------------------------------------*/
+#include "EX.h"
 /* USER CODE BEGIN Includes */
 
 /* USER CODE END Includes */
@@ -93,6 +94,7 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
