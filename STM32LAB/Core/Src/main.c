8d7bce03fc7767be241f317485267597ee628949
DESKTOP-M78FRR5\ASUS <caothanhtien2004@gmail.com>
2024-10-12 00:51:24 +0700

    init Ex_7

--- STM32LAB/Core/Src/main.c
+++ STM32LAB/Core/Src/main.c
@@ -64,7 +64,20 @@ static void MX_GPIO_Init(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+void clearAllClock() {
+		  HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, SET);
+		  HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, SET);
+		  HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, SET);
+		  HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, SET);
+		  HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, SET);
+		  HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, SET);
+		  HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, SET);
+		  HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, SET);
+		  HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, SET);
+		  HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin, SET);
+		  HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, SET);
+		  HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin, SET);
+	}
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -93,6 +106,7 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
